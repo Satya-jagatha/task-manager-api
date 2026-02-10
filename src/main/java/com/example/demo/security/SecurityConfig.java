c95dd4671d2829a434383f565a3c8925914ace1d
satya-jagatha <satyasivaram2000@gmail.com>
2026-02-10 11:43:18 +0530

    security class also modified

--- src/main/java/com/example/demo/security/SecurityConfig.java
+++ src/main/java/com/example/demo/security/SecurityConfig.java
@@ -2,16 +2,21 @@ package com.example.demo.security;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig 
 {
+	@Autowired
+	private JwtFilter jwtFilter;
 	@Bean
 	public SecurityFilterChain filterchain(HttpSecurity http) throws Exception
 	{
@@ -19,7 +24,12 @@ public class SecurityConfig
         .csrf(csrf -> csrf.disable())
         .authorizeHttpRequests(auth -> auth
             .anyRequest().permitAll()
-        );
+        )
+        .sessionManagement(sess -> sess
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
        // .cors(cors -> {}); // enable CORS if needed
     return http.build();
 	}
